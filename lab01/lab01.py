08bf0f98f0bb1377aea1d1b6e199c3a0964b8633
dnainar <77752682+dnainar@users.noreply.github.com>
2021-01-26 14:15:14 -0600

    Update lab01.py
    fixed syntax on line 49

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -46,7 +46,7 @@ def test1():
 def multiples_of_3_and_5(n):
     sum = 0
     for i in range (1, n):
-        if (i % 3 == 0 || i % 5 == 0):
+        if ((i % 3 == 0) or (i % 5 == 0)):
             sum = sum + i
     return sum            
 
