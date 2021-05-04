a8b237ee18739ac67de1926a7b1bd59c712b7375
dnainar <77752682+dnainar@users.noreply.github.com>
2021-05-04 14:43:28 -0500

    Update lab11.py

--- lab11/lab11.py
+++ lab11/lab11.py
@@ -6,18 +6,50 @@ def quicksort(lst,pivot_fn):
 
 def qsort(lst,low,high,pivot_fn):
     ### BEGIN SOLUTION
+    if low < high:
+      pivot = pivot_fn(lst, low, high)
+      pivotVal = lst[pivot]
+      highIndex = high + 1
+      lowIndex = low - 1
+
+      while lowIndex < highIndex:
+        highIndex = highIndex - 1
+        while highIndex > 0 and lst[highIndex] > pivotVal:
+          highIndex = highIndex - 1
+        
+        lowIndex = lowIndex + 1
+        while lowIndex <= high and lst[lowIndex] < pivotVal:
+          lowIndex = lowIndex + 1
+
+        if lowIndex < highIndex:
+          lst[lowIndex], lst[highIndex] = lst[highIndex], lst[lowIndex]
+
+      qsort(lst, low, highIndex, pivot_fn)
+      qsort(lst, highIndex + 1, high, pivot_fn)  
     ### END SOLUTION
 
 def pivot_first(lst,low,high):
     ### BEGIN SOLUTION
+    return low
     ### END SOLUTION
 
 def pivot_random(lst,low,high):
     ### BEGIN SOLUTION
+    rand = random.randrange(low, high)
+    return rand
     ### END SOLUTION
 
 def pivot_median_of_three(lst,low,high):
     ### BEGIN SOLUTION
+    avg = (low + high) // 2
+    medians = [lst[low], lst[avg], lst[high]]
+    quicksort(medians, pivot_first)
+    if medians[1] == lst[high]:
+      return high
+    elif medians[1] == lst[low]:
+      return low
+    else:
+      return avg
     ### END SOLUTION
 
 ################################################################################
